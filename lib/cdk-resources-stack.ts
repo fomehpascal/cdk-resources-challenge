7c2d7265d12d2852b39ef39be982a62f7d4ec360
Pascal Nde <fomehpascal@yahoo.com>
2025-08-18 22:48:22 -0400

    made code changes to the oidcRole set up

--- lib/cdk-resources-stack.ts
+++ lib/cdk-resources-stack.ts
@@ -1,17 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
-import { Bucket, BucketEncryption, CfnBucket } from 'aws-cdk-lib/aws-s3';
-import { RemovalPolicy } from 'aws-cdk-lib';
+import { Bucket, BucketEncryption} from 'aws-cdk-lib/aws-s3';
+import { RemovalPolicy, CfnOutput} from 'aws-cdk-lib';
+import { Key } from 'aws-cdk-lib/aws-kms';
+import { Role, OpenIdConnectProvider, WebIdentityPrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { CfnGitHubRepository } from 'aws-cdk-lib/aws-codestar';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
 
 // define the properties passed to s3 stack construct 
 export interface SecureBucketProps extends cdk.StackProps {
   projectId: string;
   enableVersioning?: boolean;
   enableEncryption?: boolean;
-  blockPublicAccess?: boolean
+  blockPublicAccess?: boolean;
+  githubRepoOwner: string;
+  githubRepoName: string;
   githubRepo?: string; // e.g. 'yourorg/yourrepo'
 }
 
@@ -27,27 +33,45 @@ export class CdkResourcesStack extends cdk.Stack {
     //requirement: bucket name is prefixed with a project identifier
     const bucketNamePrefix = props.projectId;
     const bucketId = `${bucketNamePrefix}-my-bucket`; 
-    
+    this.bucketName = bucketId
 
-    // create an s3 bucket 
+    //  Bucket supports optional versioning and KMS encryption (pass in as construct props). 
     const bucket = new Bucket(this, 's3SecureBucket', {
-      bucketName: bucketId.toLowerCase(),
-      versioned: props.enableVersioning ?? true,  // enable versioing 
-      encryption: props.enableEncryption ? BucketEncryption.S3_MANAGED // KMS encryption if specified, else S3 Managed
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
-      
-      // Always block public access for security
+      bucketName: this.bucketName.toLowerCase(),
+      removalPolicy: RemovalPolicy.DESTROY, // Automatically destroy bucket and contents on stack deletion
+      versioned: props?.enableVersioning ?? true,                        // enable versioning 
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // Block all public access
+      encryption: BucketEncryption.S3_MANAGED // Enable S3-managed encryption
+    });
+
+    this.bucketName = bucket.bucketName; // Store bucket name for output
+
+    const oidcProvider = OpenIdConnectProvider.fromOpenIdConnectProviderArn(this, 'GitHubOIDCProvider', 
+      `arn:aws:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`
+    );
+
+    const oidcRole = new Role(this, 'GitHubActionsOICDRole', {
+      assumedBy: new WebIdentityPrincipal(oidcProvider.openIdConnectProviderArn, {
+        StringLike: {
+          'token.actions.githubusercontent.com:sub': `repo:${props.githubRepoOwner}/${props.githubRepoName}:*`,
+        },
+      }),
+      managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess')], // Adjust permissions as needed
+    });
+
+    this.oidcRoleArn = oidcRole.roleArn; // Store OIDC role ARN for output
 
 
+    // 3. Output the OIDC Role ARN and the bucket name
+    new CfnOutput(this, 'BucketName', {
+      value: this.bucketName,
+      description: 'The name of the S3 bucket',
+    });
 
-    })
+    new CfnOutput(this, 'OidcRoleArn', {
+      value: this.oidcRoleArn,
+      description: 'The ARN of the GitHub OIDC role for deployments',
+    });
 
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkResourcesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
